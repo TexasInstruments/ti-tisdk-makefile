82eb7a51fb49162d907cc4a8ffa47aeb0cdcedd2
Paresh Bhagat <p-bhagat@ti.com>
2026-03-09 14:14:43 +0530

    configs: platforms: am62dxx-evm: Update toolchain paths and dts support
    - Update cross compiler paths to use LINUX_DEVKIT_PATH and
      K3_R5_LINUX_DEVKIT_PATH variables
    - Add ti/k3-am62a7-sk-e3-max-opp device tree to supported list
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

--- configs/platforms/am62dxx-evm.mk
+++ configs/platforms/am62dxx-evm.mk
@@ -6,9 +6,9 @@ PLATFORM?=am62dxx-evm
 YOCTO_MACHINE?=am62dxx-evm
 ARCH=arm64
 
-#Set cross compilers
-export CROSS_COMPILE=$(TI_SDK_PATH)/linux-devkit/sysroots/x86_64-arago-linux/usr/bin/aarch64-oe-linux/aarch64-oe-linux-
-export CROSS_COMPILE_ARMV7=$(TI_SDK_PATH)/k3r5-devkit/sysroots/x86_64-arago-linux/usr/bin/arm-oe-eabi/arm-oe-eabi-
+#ARM Toolchains
+export CROSS_COMPILE=$(LINUX_DEVKIT_PATH)/sysroots/x86_64-arago-linux/usr/bin/aarch64-oe-linux/aarch64-oe-linux-
+export CROSS_COMPILE_ARMV7=$(K3_R5_LINUX_DEVKIT_PATH)/sysroots/x86_64-arago-linux/usr/bin/arm-oe-eabi/arm-oe-eabi-
 
 #Default CC value to be used when cross compiling.  This is so that the
 #GNU Make default of "cc" is not used to point to the host compiler
@@ -19,7 +19,7 @@ UBOOT_MACHINE=am62dx_evm_a53_defconfig
 UBOOT_MACHINE_R5=am62dx_evm_r5_defconfig
 MKIMAGE_DTB_FILE=a53/dts/upstream/src/arm64/ti/k3-am62d2-evm.dtb
 
-KERNEL_DEVICETREE_PREFIX=ti/k3-am62d2|ti/k3-am62a7-sk-edgeai|ti/k3-am62a7-sk-rpi-hdr-ehrpwm|ti/k3-am62x-sk
+KERNEL_DEVICETREE_PREFIX=ti/k3-am62d2|ti/k3-am62a7-sk-edgeai|ti/k3-am62a7-sk-rpi-hdr-ehrpwm|ti/k3-am62x-sk|ti/k3-am62a7-sk-e3-max-opp
 
 TI_LINUX_FIRMWARE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)
 UBOOT_ATF=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl31.bin
