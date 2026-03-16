43ef96d652a9ae818a2f362611d2431a17c75e2a
Yogesh Hegde <y-hegde@ti.com>
2026-03-16 17:42:44 +0530

    makerules: resolve glob patterns in KERNELDIR paths for ti-img-rogue-driver
    The ti-img-rogue-driver build fail when LINUXKERNEL_INSTALL_DIR
    contains glob patterns (e.g., "linux-*"). These patterns are not
    expanded when passed as make variables to nested builds, causing
    KERNELDIR to point to non-existent paths.
    
    Fix by using $(shell ls -d ...) to expand the glob pattern before
    passing KERNELDIR to the sub-make invocations. This ensures the
    full resolved path is used in both build and clean targets.
    
    The fix has been extended to other variables preemptively.
    
    Signed-off-by: Yogesh Hegde <y-hegde@ti.com>

--- configs/setup/tisdk-installer.mk
+++ configs/setup/tisdk-installer.mk
@@ -7,14 +7,14 @@ SDK_PATH_TARGET=$(LINUX_DEVKIT_PATH)/sysroots/aarch64-oe-linux
 SDK_PATH_HOST=$(LINUX_DEVKIT_PATH)/sysroots/x86_64-arago-linux
 
 # The source directories for each component
-ARM_BENCHMARKS_SRC_DIR=$(TI_SDK_PATH)/example-applications/*arm-benchmarks*
-AM_SYSINFO_SRC_DIR=$(TI_SDK_PATH)/example-applications/*am-sysinfo*
-OPROFILE_SRC_DIR=$(TI_SDK_PATH)/example-applications/*oprofile-example*
-CRYPTODEV_SRC_DIR=$(TI_SDK_PATH)/board-support/extra-drivers/cryptodev*
-IMG_ROGUE_SRC_DIR=$(TI_SDK_PATH)/board-support/extra-drivers/ti-img-rogue-driver*
-UBOOT_SRC_DIR=$(TI_SDK_PATH)/board-support/ti-u-boot*
-KIG_SRC_DIR=$(TI_SDK_PATH)/board-support/k3-image-gen-*
-LINUXKERNEL_INSTALL_DIR=$(TI_SDK_PATH)/board-support/ti-linux-kernel*
-TFA_SRC_DIR=$(TI_SDK_PATH)/board-support/trusted-firmware-a*
+ARM_BENCHMARKS_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/example-applications/*arm-benchmarks*)
+AM_SYSINFO_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/example-applications/*am-sysinfo*)
+OPROFILE_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/example-applications/*oprofile-example*)
+CRYPTODEV_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/extra-drivers/cryptodev*)
+IMG_ROGUE_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/extra-drivers/ti-img-rogue-driver*)
+UBOOT_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/ti-u-boot*)
+KIG_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/k3-image-gen-*)
+LINUXKERNEL_INSTALL_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/ti-linux-kernel*)
+TFA_SRC_DIR:=$(shell ls -d $(TI_SDK_PATH)/board-support/trusted-firmware-a*)
 
 export TI_SECURE_DEV_PKG=$(TI_SDK_PATH)/board-support/core-secdev-k3
